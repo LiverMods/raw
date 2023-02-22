fdd69eb71982aa2a7fc6a9c750c4ab8d3844f708
Liver zMods | zRe ʳᵉⁱ ᵖᵉˡᵉ <121512773+LiverMods@users.noreply.github.com>
2023-02-22 15:37:40 -0300

    Update RBLX.lua

--- Natural/Disaster/RBLX.lua
+++ Natural/Disaster/RBLX.lua
@@ -1,1 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LiverMods/raw/main/Natural/Disaster/2023.lua", true))()
+--[[
+       Feito por Liver zMods
+       Made by Liver zMods
+       Realizado por Liver zMods
+       YouTube link: https://youtube.com/@liver_mods
+--]]
